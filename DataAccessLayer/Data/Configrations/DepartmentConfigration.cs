eed7c581bae5f0ebd792dfed01dbf1faf61c5856
GHK <gamalhanafi26@gmail.com>
2025-03-24 19:39:43 +0200

    Create DAL & BLL then make Base Entity & Department Model with it's Configrations

---
+++ DataAccessLayer/Data/Configrations/DepartmentConfigration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Data.Configrations
+{
+    internal class DepartmentConfigration : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D=>D.Id).UseIdentityColumn(10,10);
+            builder.Property(D => D.Name).HasColumnType("VarChar(20)");
+            builder.Property(D => D.Code).HasColumnType("VarChar(20)");
+            builder.Property(D => D.CreatedOn).HasDefaultValueSql("GetDAte()");
+            builder.Property(D => D.LastModifiedOn).HasComputedColumnSql("GetDate()");
+        }
+    }
+}
---
+++ DataAccessLayer/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Data.Contexts
+{
+    internal class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Connection String");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
