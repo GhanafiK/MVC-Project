b46a74cb3f6f64a3d8cba0c17cd1a644a1e7ff50
GHK <gamalhanafi26@gmail.com>
2025-03-25 07:11:17 +0200

    Make the Action Create in Department Controller

--- BusinessLogicLayer/DTOs/CreatedDepartmentDTO.cs
+++ BusinessLogicLayer/DTOs/CreatedDepartmentDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,10 @@ namespace BusinessLogicLayer.DTOs
     public class CreatedDepartmentDTO
     {
         public DateOnly DateOfCreation { get; set; }
+        [Required(ErrorMessage ="Name is Reqired :(")]
         public string Name { get; set; } = string.Empty;
+        [Required]
+        [Range(100,10000)]
         public string Code { get; set; } = string.Empty;
         public string? Description { get; set; }
     }
--- PresentationLayer/Controllers/DepartmentsController.cs
+++ PresentationLayer/Controllers/DepartmentsController.cs
@@ -1,14 +1,44 @@
-﻿using BusinessLogicLayer.Services;
+﻿using BusinessLogicLayer.DTOs;
+using BusinessLogicLayer.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PresentationLayer.Controllers
 {
-    public class DepartmentsController(IDepartmentService _departmentService):Controller
+    public class DepartmentsController(IDepartmentService _departmentService,ILogger<DepartmentsController> _logger,IWebHostEnvironment _environment):Controller
     {
         public IActionResult Index()
         {
             var departments = _departmentService.GetAllDepartments();
             return View(departments);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreatedDepartmentDTO data)
+        {
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    int Result = _departmentService.AddDepartment(data);
+                    if (Result > 0)
+                        return RedirectToAction(nameof(Index));
+                    else
+                        ModelState.AddModelError(string.Empty, "Can't Add Department");
+                }
+                catch (Exception ex)
+                {
+                    if (_environment.IsDevelopment())
+                        ModelState.AddModelError(string.Empty, ex.Message);
+                    else
+                        _logger.LogError(ex.Message);
+                }
+            }
+            return View(data);
+        }
     }
+
 }
