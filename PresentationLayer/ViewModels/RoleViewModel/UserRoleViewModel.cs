91a4bcf9d6fc3be2fd83f7230165b13a132e3a32
GHK <gamalhanafi26@gmail.com>
2025-04-21 19:10:08 +0200

    Update (Create - Edit) Role

--- PresentationLayer/ViewModels/RoleViewModel/UserRoleViewModel.cs
+++ PresentationLayer/ViewModels/RoleViewModel/UserRoleViewModel.cs
@@ -6,8 +6,8 @@
         public string UserName { get; set; }
         public bool IsSelected { get; set; }
 
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
+        public string? FirstName { get; set; } 
+        public string? LastName { get; set; } 
 
         public string? Email { get; set; }
     }
